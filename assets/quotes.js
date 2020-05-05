a6944333ee80063c2fbc46510925d02954608dc1
nikiindianto <nikiindianto7@gmail.com>
2020-05-05 17:26:46 +0700

    Upload di Github

---
+++ assets/quotes.js
@@ -0,0 +1,10 @@
+// prettier-ignore
+export default [
+    { author: "By. Niki Indianto", quotes: "Semangat kerjanya" },
+    { author: "By. Niki Indianto", quotes: "Jangan menyerah" },
+    { author: "By. Niki Indianto", quotes: "Kerja yang bener" },
+    { author: "By. Niki Indianto", quotes: "Biar bisa mengelar dia" },
+    { author: "By. Niki Indianto", quotes: "Biar bisa 4646" },
+    { author: "By. Niki Indianto", quotes: "Tetap semangat" },
+    { author: "By. Niki Indianto", quotes: "Good Luck!" },
+];
---
+++ assets/variables.js
@@ -0,0 +1,10 @@
+export default {
+  nicknames: ["Habib Mustofa", "", "Habib", "Habib X Ely", "Habib X Khadijah"],
+  greetings: {
+    evening: "Good Evening",
+    afternoon: "Good Afternoon",
+    day: "Good Day",
+    morning: "Good Morning",
+    night: "Good Night"
+  }
+};
